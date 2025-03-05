3ed6ee5a2f3f366cbd7a7e675c55974de3f62949
elcrmt <eliottcor2005@gmail.com>
2025-03-05 14:32:41 +0100

    done

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,29 @@ class Constants {
   // API URL
   static const String apiUrl = 'http://localhost/api';
   
+  // Image paths
+  static const String profileImagePath = 'uploads/profiles';
+  static const String horseImagePath = 'uploads';
+  
   // Other constants
   static const int passwordMinLength = 6;
-  static const String profileImagePath = 'uploads/profiles';
+  
+  // Horse constants
+  static const Map<String, String> genderLabels = {
+    'male': 'Mâle',
+    'female': 'Femelle',
+    'gelding': 'Hongre',
+  };
+  
+  static const Map<String, String> specialtyLabels = {
+    'dressage': 'Dressage',
+    'jumping': 'Saut d\'obstacle',
+    'endurance': 'Endurance',
+    'eventing': 'Complet',
+  };
+  
+  static const Map<String, String> relationshipLabels = {
+    'owner': 'Propriétaire',
+    'half_pension': 'Demi-pension',
+  };
 }
